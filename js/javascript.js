f6a1b486ed99c03384cd79fe733742025529e8b8
lan-mi <milan221088@yahoo.com>
2018-03-18 02:10:32 +0100

    add nav
    adding navigation

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
+function sandwich(x) {
+    x.classList.toggle("change")
+}
 
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
