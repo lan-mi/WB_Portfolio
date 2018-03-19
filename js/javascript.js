b548e95253c8359c1c1542e7c259d8249291354f
lan-mi <milan221088@yahoo.com>
2018-03-19 02:21:34 +0100

    adding new stuff
    Adding new stuff

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,3 @@
-function sandwich(x) {
-    x.classList.toggle("change")
-}
-
-
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
@@ -10,3 +5,7 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+$("#test").click(function () {
+    $("p").remove();
+});
