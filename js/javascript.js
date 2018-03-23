25ee99cfedd6c8770e3c45d7eb8b4a181a174840
lan-mi <milan221088@yahoo.com>
2018-03-23 23:56:19 +0100

    Animation, Portfolio Page
    Adding animation to menu button, adding content to portfolio page

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+/* Fullsreen navigation */
+
 function openNav() {
     document.getElementById('myNav').style.width = '100%';
 }
@@ -6,27 +8,36 @@ function closeNav() {
     document.getElementById('myNav').style.width = '0%';
 }
 
-/* Sandwich menu adding and removing class */
+/* Sandwich menu animate on page load */
 
-jQuery(document).ready(function ($) {
-    $('#menu-toggle').hover(
-        function () {
-            $('.menu1').addClass('active1');
-            $('.menu2').addClass('active2');
-            $('.menu3').addClass('active3');
-        },
-        function () {
-            $('.menu1').removeClass('active1');
-            $('.menu2').removeClass('active2');
-            $('.menu3').removeClass('active3');
-        }
-    )
+$(document).ready(function () {
+    $('#menu1').stop(true, true).delay(2500).animate({
+        width: "50px"
+    }, 1000).animate({
+        width: "100px"
+    }, 1000).animate({
+        width: "40px"
+    });
 });
 
-$('#menu-toggle').mouseleave(function () {
-    $('.active1').addClass('menu1');
-    $('.active2').addClass('menu2');
-    $('.active3').addClass('menu3');
+$(document).ready(function () {
+    $('#menu2').stop(true, true).delay(2000).animate({
+        width: "50px"
+    }, 1000).animate({
+        width: "100px"
+    }, 1000).animate({
+        width: "40px"
+    });
+});
+
+$(document).ready(function () {
+    $('#menu3').stop(true, true).delay(3000).animate({
+        width: "50px"
+    }, 1000).animate({
+        width: "100px"
+    }, 1000).animate({
+        width: "40px"
+    });
 });
 
 /* Adding active class to curent page link */
@@ -38,31 +49,6 @@ $(document).ready(function () {
         }
     });
 });
-
-/* main menu text animation */
-
-var textArray = ["learning!", "new technologies!", "web development!"];
-var index = 0;
-setInterval(function () {
-    $("#main-animate").animate({
-        opacity: 0
-    }, function () {
-        if (textArray.length > index) {
-            $(this).text(textArray[index]).animate({
-                opacity: 1
-            })
-            index++;
-        } else
-            index = 0;
-    });
-}, 3000);
-
-/* Animate name on page load */
-
-$('#a').animate({
-    right: 200
-});
-
 
 /* Animate name div */
 
@@ -93,12 +79,81 @@ $(document).ready(function () {
 
 });
 
+/* Main menu text animation */
 
+var textArray = ["learning!", "new technologies!", "web development!"];
+var index = 0;
+setInterval(function () {
+    $("#main-animate").animate({
+        opacity: 0
+    }, function () {
+        if (textArray.length > index) {
+            $(this).text(textArray[index]).animate({
+                opacity: 1
+            })
+            index++;
+        } else
+            index = 0;
+    });
+}, 3000);
 
+/* Animate '.name-border' div */
 
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.top-border').animate({
+            width: "80%"
+        }, 500);
+    }, 3500);
+});
+
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.bottom-border').animate({
+            width: "70%"
+        }, 500);
+    }, 3500);
+});
+
+/* Animating sandwich menu on hover */
+
+$(document).ready(function () {
+    $('#menu-toggle').hover(function () {
+        $('.menu1').animate({
+            width: "30px"
+        }, 200);
+        $('.menu2').animate({
+            width: "25px"
+        }, 200);
+        $('.menu3').animate({
+            width: "35px"
+        }, 200);
+
+    }, function () {
+        $('.menu1').animate({
+            width: "40px"
+        }, 200);
+        $('.menu2').animate({
+            width: "40px"
+        }, 200);
+        $('.menu3').animate({
+            width: "40px"
+        }, 200);
+    });
+});
+
+/*  ----------------- */
 
 
 
 
 
 /* */
+
+
+
+
+
+
+
+/*  ---------------   */
